b65fdd752e24679e8de41caca4c44f920f7fb281
Matyáš Hiřman <matyashirman@Matyas-MBP.home>
2024-07-02 14:19:46 +0200

    Add cards.xlsx and update Board component to read it

--- src/components/Board.js
+++ src/components/Board.js
@@ -29,7 +29,7 @@ function Board() {
 
   useEffect(() => {
     // Načtení karet z Excel souboru
-    fetch('/cards.xlsx')
+    fetch('public/cards.xlsx')
       .then(response => response.arrayBuffer())
       .then(data => {
         const workbook = XLSX.read(data, { type: 'array' });
